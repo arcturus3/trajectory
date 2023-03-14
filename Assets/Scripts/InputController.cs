a1cf786084898ca2199cb0ac0bd6bed18444e3a2
Arti Schmidt <artischmidt3@gmail.com>
2023-03-14 13:33:09 -0400

    Add specific events to InputController

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,120 +1,67 @@
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class InputController : MonoBehaviour
 {
-    public InputAction pointerPosition;
-    public InputAction pointerMove;
-    public InputAction panButton;
-    public InputAction orbitButton;
-    public InputAction setWaypointButton;
-    public GameObject waypointMarker;
-    private GameObject camera;
-    private GameObject cameraAnchor;
+    public static event Action<InputMode> onInputModeChange;
+    public static event Action<Vector2, Vector2> onPointerMove;
+    public static event Action onPanStart;
+    public static event Action onPanEnd;
+    public static event Action onOrbitStart;
+    public static event Action onOrbitEnd;
+    public static event Action onWaypointDrop;
+
     private InputMode inputMode;
 
-    private GameObject waypointPreview;
-    private List<GameObject> waypoints;
-
     private void OnEnable() {
-        pointerMove.Enable();
-        panButton.Enable();
-        orbitButton.Enable();
-        setWaypointButton.Enable();
-
         UI.onInputModeChange += SetInputMode;
     }
 
     private void OnDisable() {
-        pointerMove.Disable();
-        panButton.Disable();
-        orbitButton.Disable();
-        setWaypointButton.Disable();
-    }
-
-    private void Start() {
-        camera = Camera.main.gameObject;
-        cameraAnchor = camera.transform.parent.gameObject;
-        waypointPreview = Instantiate(waypointMarker);
+        UI.onInputModeChange -= SetInputMode;
     }
 
     private void Update() {
+        onPointerMove.Invoke(
+            Mouse.current.position.value,
+            Mouse.current.delta.value
+        );
         switch (inputMode) {
             case InputMode.Navigate:
-                HandlePan();
-                HandleOrbit();
-                HandleZoom();
+                if (Mouse.current.leftButton.wasPressedThisFrame)
+                    onPanStart.Invoke();
+                if (Mouse.current.leftButton.wasReleasedThisFrame)
+                    onPanEnd.Invoke();
+                if (Mouse.current.rightButton.wasPressedThisFrame)
+                    onOrbitStart.Invoke();
+                if (Mouse.current.rightButton.wasReleasedThisFrame)
+                    onOrbitEnd.Invoke();
                 break;
             case InputMode.Edit:
-                HandleSetWaypoint();
+                if (Mouse.current.leftButton.wasReleasedThisFrame)
+                    onWaypointDrop.Invoke();
                 break;
         }
     }
 
     private void SetInputMode(InputMode mode) {
         inputMode = mode;
-        waypointPreview.SetActive(mode == InputMode.Edit);
-    }
-
-    private void HandlePan() {
-        // TODO: smoothing mouse input
-        // TODO: raycast to scene to determine drag delta
-        if (panButton.IsPressed()) {
-            Plane groundPlane = new Plane(Vector3.up, 0);
-            float panSpeed = 0.025f;
-            Vector2 moveDelta = pointerMove.ReadValue<Vector2>();
-            Vector3 panDelta = moveDelta.x * GetPanRight() + moveDelta.y * GetPanForward();
-            cameraAnchor.transform.Translate(-panSpeed * panDelta, Space.World);
-        }
-    }
-
-    private void HandleOrbit() {
-        // TODO: x axis rotation limits
-        if (orbitButton.IsPressed()) {
-            Vector2 moveDelta = pointerMove.ReadValue<Vector2>();
-            float orbitSpeed = 0.1f;
-            Vector3 orbitDelta = orbitSpeed * new Vector3(-moveDelta.y, moveDelta.x, 0);
-            cameraAnchor.transform.eulerAngles += orbitDelta;
-            Mathf.Clamp(cameraAnchor.transform.eulerAngles.x, 0, 90);
-        }
-    }
-
-    private void HandleZoom() {
-
-    }
-
-    private void HandleSetWaypoint() {
-        Vector2 position = pointerPosition.ReadValue<Vector2>();
-        Ray ray = Camera.main.ScreenPointToRay(position);
-        RaycastHit hit;
-        bool success = Physics.Raycast(ray, out hit);
-        if (success) {
-            waypointPreview.SetActive(true);
-            waypointPreview.transform.position = hit.point;
-            if (setWaypointButton.WasPressedThisFrame()) {
-                GameObject waypoint = Instantiate(waypointMarker);
-                waypoint.transform.position = hit.point;
-                waypoints.Add(waypoint);
-            }
-        }
-        else {
-            waypointPreview.SetActive(false);
-        }
-    }
-
-    private Vector3 GetPanForward() {
-        float angle = cameraAnchor.transform.rotation.eulerAngles.y;
-        return Quaternion.Euler(0, angle, 0) * Vector3.forward;
-    }
-
-    private Vector3 GetPanRight() {
-        float angle = cameraAnchor.transform.rotation.eulerAngles.y;
-        return Quaternion.Euler(0, angle, 0) * Vector3.right;
+        onInputModeChange.Invoke(mode);
     }
 }
 
 public enum InputMode {
     Navigate,
     Edit
+}
+
+public class InpuState {
+    public InputMode inputMode;
+    public Vector2Control pointerPosition;
+    public Vector2Control pointerDelta;
+    public ButtonControl panButton;
+    public ButtonControl orbitButton;
+    public ButtonControl waypointDropButton;
 }
