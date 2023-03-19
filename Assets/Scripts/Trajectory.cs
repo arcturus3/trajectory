c828b068eb35b27628929ac40ecaae0f91922124
Arti Schmidt <artischmidt3@gmail.com>
2023-03-19 12:22:09 -0400

    Finish trajectory message types

--- Assets/Scripts/Trajectory.cs
+++ Assets/Scripts/Trajectory.cs
@@ -1,54 +1,42 @@
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Linq;
 using UnityEngine;
 
 public class Trajectory {
     private Client client;
     private List<Waypoint> waypoints;
+    private int id;
 
     public Trajectory(Client client, List<Waypoint> waypoints) {
         this.client = client;
         this.waypoints = waypoints;
-        Dictionary<
-        client.SendRequest(?);
-        /*
-        {
-            command: 'generate',
-            args: {
-                waypoints: [
-                    {position: [x, y, z]},
-                    {position: [x, y, z]},
-                    ...
-                ]
-            }
-        }
-        {
-            trajectory_id: x
-        }
-
-        {
-            command: 'query',
-            args: {
-                trajectory_id: x,
-                time: x
-            }
-        }
-        {
-            position: [x, y, z],
-            normal: [x, y, z]
-        }
-        */
+        GenerateRequest request = new GenerateRequest() {
+            Waypoints = waypoints.Select(waypoint => new MessageWaypoint{
+                Position = new List<float>() {
+                    waypoint.position.x,
+                    waypoint.position.y,
+                    waypoint.position.z
+                }
+            }).ToList()
+        };
+        string requestString = JsonSerializer.Serialize(request);
+        string responseString = client.SendRequest(requestString);
+        GenerateResponse response = JsonSerializer.Deserialize<GenerateResponse>(responseString);
+        id = response.TrajectoryId;
     }
 
     public Vector3 GetPosition(float time) {
-        {
-            'get_position',
-
-        }
-        client.SendRequest(?);
-    }
-
-    public Quaternion GetRotation(float time) {
-        client.SendRequest(?);
+        QueryRequest request = new QueryRequest() {
+            TrajectoryId = id,
+            Time = time
+        };
+        string requestString = JsonSerializer.Serialize(request);
+        string responseString = client.SendRequest(requestString);
+        QueryResponse response = JsonSerializer.Deserialize<QueryResponse>(responseString);
+        float x = response.Position[0];
+        float y = response.Position[1];
+        float z = response.Position[2];
+        return new Vector3(x, y, z);
     }
 }
